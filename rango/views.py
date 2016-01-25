c8e14b08eceb8f29fa89b5725b737b6aac80be7a
Zachary McNellis <zmcnell@clemson.edu>
2016-01-25 09:52:31 -0500

    First commit

---
+++ rango/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
